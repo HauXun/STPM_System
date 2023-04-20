801a0a34825191a60284689e181835422db0ac27
hauxun <hautrieu65@gmail.com>
2023-04-20 23:32:12 +0700

    base core/collections structure

---
+++ service/Stpm.Core/Collections/PaginationResult.cs
@@ -0,0 +1,16 @@
+﻿using Stpm.Core.Contracts;
+
+namespace Stpm.Core.Collections;
+
+public class PaginationResult<T>
+{
+	public IEnumerable<T> Items { get; set; }
+
+	public PagingMetadata Metadata { get; set; }
+	
+	public PaginationResult(IPagedList<T> pagedList)
+	{
+		Items = pagedList;
+		Metadata = new PagingMetadata(pagedList);
+	}
+}
